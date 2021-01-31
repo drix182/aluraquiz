8e34e637adac0944a802509dd0262abb67ce20de
Drix <drix182@gmail.com>
2021-01-31 15:36:13 +0000

    codigo final da primeira aula da imersao react nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+     <QuizBackground backgroundImage={db.bg}>
+         <QuizContainer>
+             <Widget>
+                  <Widget.Header>
+                      <h1>London Calling</h1>
+                  </Widget.Header>
+                 <Widget.Content>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea harum, repellat ullam cupiditate eligendi pariatur ipsa perferendis quia, quos similique, reiciendis sit quidem a ducimus consectetur. Eligendi accusantium maxime numquam?</p>
+                 </Widget.Content>
+                 
+             </Widget>
+
+              <Widget>
+                  <Widget.Header>
+                      <h1>Quizes da Galera</h1>
+                  </Widget.Header>
+                  <Widget.Content>
+                      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea harum, repellat ullam cupiditate eligendi pariatur ipsa perferendis quia, quos similique, reiciendis sit quidem a ducimus consectetur. Eligendi accusantium maxime numquam?</p>
+                  </Widget.Content>
+              </Widget>
+              <Footer />
+         </QuizContainer>
+         <GitHubCorner projectUrl='https://github.com/drix182'></GitHubCorner>
+
+      </QuizBackground>
+  )
 }
